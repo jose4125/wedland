ce3e73f3f35a8147059bfc032d968da8f3bd96b0
jose david lombana <jose4125@gmail.com>
2012-11-01 20:03:06 -0500

    structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wedland::Application.config.secret_token = '38852d02829ff97ba8375ec48836d59bbe324e1aeff20ddc94e2d1aea9b51580a73f4efb86e2699589b97d1009f95d387e7f1bbe97533917d4b9296a7bb7ee48'
